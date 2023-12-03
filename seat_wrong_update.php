e9ac3dbd5a26c2fe5f300f0ec157f70266df6e45
Shagufta Sajid <119622939+Shagufta-Sajid@users.noreply.github.com>
2023-12-04 02:38:56 +0600

    User seat update

---
+++ seat_wrong_update.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body  {
+  background-image: url("local_background.png");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+}
+button {
+  background-color:  #f55d0494;
+  color: white;
+  padding: 14px 20px;
+  margin: 50px 0px;
+  border: none;
+  cursor: pointer;
+  width: 20%;
+}
+
+button:hover {
+  opacity: 0.8;
+}
+</style>
+</head>
+<body>
+
+<h1> Something went wrong, please Try again carefully.. </h1>
+
+
+<a href="admin_seat_update.php">
+<button type="submit">Try again </button>
+</a>
+
+  
+</form>
+</body>
+</html>
