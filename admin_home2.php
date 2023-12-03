cd2e3b3cf1abef00323a5b61f08d1e570217c73b
Shagufta Sajid <119622939+Shagufta-Sajid@users.noreply.github.com>
2023-12-04 00:31:11 +0600

    Admin homepage created

---
+++ admin_home2.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+body  {
+  background-image: url("uhms_admin_background_picc.png");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+}
+.button {
+  background-color: #f55d0494;
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 20px;
+  margin: 10px 10px;
+  cursor: pointer;
+  
+  
+}
+.button {border-radius: 5px;}
+.button:hover {
+  background-color: #6dbe7b;
+}
+</style>
+</head>
+<body>
+<a href="admin_index_home.php" class="button">Home</a>
+  
+  <a href="hostel_info.php" class="button">Hostel Information</a>
+  <a href="info_student.php" class="button">Student Information</a>
+  <a href="student_info_admin.php" class="button">Signed Up</a>
+  <a href="admin_payments.php" class="button">Payments </a>
+  <a href="admin_update.php" class="button">Updates</a>
+  <a href="admin_notice1.php" class="button">Notice</a>
+
+
+
+</body>
+</html>
---
+++ admin_index_home.php
@@ -0,0 +1,42 @@
+<?php 
+include('admin_home1.php');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+h1 {
+  font-size: 40px;
+  padding: 50px;
+  margin: -50px 20px;
+  text-align: justify;
+  font-family: Tahoma, Verdana, sans-serif;
+  text-shadow: 1px 1px 12px whitesmoke;
+}
+div {
+  border: 5px solid #ff9d5f;
+  padding: 50px;
+  margin: -50px 70px;
+  width: 300px;
+  height: 170px;
+  text-align: justify;
+  font-family: Tahoma, Verdana, sans-serif;
+  line-height: 2.3;
+  font-size: 150%;
+  background-color: #ebd0cb;
+  text-shadow: 1px 1px 12px whitesmoke;
+ 
+}
+
+
+</style>
+</head>
+<body>
+<h1>User Information:</h1>
+<div>
+Username: Admin  <br>
+Email: uiu15@gmail.com   <br>     
+Phone: +09604848848
+</div>
+</body>
+</html>
