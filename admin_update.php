c6c17b7a5ef3e5446827d0625c4f70ac782b4624
Shagufta Sajid <119622939+Shagufta-Sajid@users.noreply.github.com>
2023-12-04 02:42:48 +0600

    Admin part completed

---
+++ admin_update.php
@@ -0,0 +1,49 @@
+<?php
+include("admin_home2.php");
+?>
+
+
+<!DOCTYPE html>
+<html>
+<style>
+
+button {
+  background-color:  #f55d0494;
+  color: white;
+  padding: 20px 20px;
+  margin: 8px 20px;
+  border: none;
+  cursor: pointer;
+  width: 10%;
+}
+
+button:hover {
+  opacity: 0.8;
+}
+
+</style>
+<body>
+<h1>For Update Payments Click On 'Payments Update' below:</h1>
+<a href="admin_pay_update.php" class="button">Payments Update</a>
+<br></br>
+<br></br>
+<br></br>
+<h1>For Update Seat Booking Information Of Hostels Click On 'Seat Booking Information Update' below:</h1>
+<a href="admin_seat_update.php" class="button">Seat Booking Information Update</a>
+<br></br>
+<br></br>
+<br></br>
+<h1>For Update Seat Availability(if anyone wants to withdraw) Of Hostels Click On 'Seat Availability Update' below:</h1>
+<a href="admin_seat_available_update.php" class="button">Seat Availability Update</a>
+
+
+
+
+
+
+</body>
+</html>
+
+
+
+
---
+++ pay&book_done.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body  {
+  background-image: url("local_background.png");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+}
+button {
+  background-color:  #f55d0494;
+  color: white;
+  padding: 14px 20px;
+  margin: 50px 0px;
+  border: none;
+  cursor: pointer;
+  width: 20%;
+}
+
+button:hover {
+  opacity: 0.8;
+}
+</style>
+</head>
+<body>
+
+<h1>Room Booked & Successfully Paid.. </h1>
+
+
+<a href="user_home_page.php">
+<button type="submit">Go to homepage</button>
+</a>
+
+  
+</form>
+</body>
+</html>
