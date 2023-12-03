1452f439c949e9b2f2cd0bc34c9dc2830d64d1de
Shagufta Sajid <119622939+Shagufta-Sajid@users.noreply.github.com>
2023-12-04 02:41:16 +0600

    Admin withdraw update

---
+++ already_withdraw_update.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body  {
+  background-image: url("local_background.png");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+}
+button {
+  background-color:  #f55d0494;
+  color: white;
+  padding: 14px 20px;
+  margin: 50px 0px;
+  border: none;
+  cursor: pointer;
+  width: 20%;
+}
+
+button:hover {
+  opacity: 0.8;
+}
+</style>
+</head>
+<body>
+
+<h1> Opps, This seat is already 'Available'</br> Try again with right information....  </h1>
+
+<a href="admin_seat_available_update.php">
+<button type="submit">Try Again</button>
+</a>
+
+  
+</form>
+</body>
+</html>
---
+++ withdraw_update_done.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body  {
+  background-image: url("local_background.png");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+}
+button {
+  background-color:  #f55d0494;
+  color: white;
+  padding: 14px 20px;
+  margin: 50px 0px;
+  border: none;
+  cursor: pointer;
+  width: 20%;
+}
+
+button:hover {
+  opacity: 0.8;
+}
+</style>
+</head>
+<body>
+
+<h1> Updated Successfully, Please go to 'Hostel Information' and check it...</h1>
+
+<a href="hostel_info.php">
+<button type="submit">Go To Hostel Information</button>
+</a>
+
+  
+</form>
+</body>
+</html>
